b386f9e3763eb4345033598ec329d73aee0e732b
OmarHoussami <omar.houssami20@gmail.com>
2021-10-03 18:05:44 +0300

    faces can now be captured in the gray scale

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ show = cv.VideoCapture(0)
 
 face_det = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
 body_det = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_fullbody_default.xml")
+
+#program loop
 while True:
   _, frame = show.read()
   
   gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+  faces = face_det.detectMultiScale(gray, 1.3, 5)
+  
   cv.imshow("Footage", frame)
   
   if cv.waitKey(1) == ord('q'):
     break
-  
+# end of loop
+
 show.release()
 cv.destroyAllWindows()
